a488c7f33a892062775e6a94d5854d434a713299
Rahul Nadella <rahulnadella@yahoo.com>
2015-03-29 11:15:21 -0400

    Initial Commit

---
+++ CoffeeKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeKit
+//
+//  Created by Rahul Nadella on 3/29/15.
+//  Copyright (c) 2015 Rahul Nadella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeeKit/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  CoffeeKit
+//
+//  Created by Rahul Nadella on 3/29/15.
+//  Copyright (c) 2015 Rahul Nadella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
