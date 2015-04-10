2ca54574b98fc6b1e15b1d1d089f862eff01b37d
Rahul Nadella <rahulnadella@yahoo.com>
2015-04-10 14:18:27 -0400

    Cleanup of AppDelegate

--- CoffeeKit/CoffeeKitAppDelegate.m
+++ CoffeeKit/CoffeeKitAppDelegate.m
@@ -41,14 +41,16 @@
     
     [self initLocation];
     
-    // Override point for customization after application launch.
+    /* Override point for customization after application launch. */
     return YES;
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -77,7 +79,9 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    /* 
+     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+     */
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -89,7 +93,9 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    /* 
+     Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+     */
 }
 
 #pragma mark - Initialization Location
